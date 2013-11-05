0f007c526ead396c1f366fe9a273083428b92261
Zach Stednick <zstednic@fhcrc.org>
2013-11-05 11:00:13 -0800

    updating functionality for reading in samples

--- sample_repo.py
+++ sample_repo.py
@@ -32,7 +32,7 @@ def init_db():
         db.commit()
 
 def get_user_id(username):
-    rv = g.db.execute('select user_id from user where username = ?',
+    rv = g.db.execute('SELECT user_id FROM user where username = ?',
                         [username]).fetchone()
     return rv[0] if rv else None
 
@@ -62,7 +62,7 @@ def before_request():
     g.db = connect_db()
     g.user = None
     if 'user_id' in session:
-        g.user = query_db('select * from user where user_id = ?',
+        g.user = query_db('SELECT * FROM user where user_id = ?',
                             [session['user_id']], one = True)
 
 @app.teardown_request
@@ -89,34 +89,37 @@ def login():
 def main():
     return render_template('index.html')
 
-
 @app.route('/all_samples', methods = ['GET', 'POST'])
 def all_samples():
 	error = None
-	entries = query_db("""select irs_id, proj_id, proj_tube_no, 
+	entries = query_db("""SELECT irs_id, proj_id, proj_tube_no, 
                         proj_cell, date_out, shipped_to,
                         sent_to, received_date from sample_movement""", one = False ) 
 	return render_template('all_samples.html', entries = entries)
 
 @app.route('/all_patients', methods = ['GET', 'POST'])
 def pt_demo():
-    entries = query_db("""select irs_id, ptdon, sample_res, sample_type,
+    entries = query_db("""SELECT irs_id, ptdon, sample_res, sample_type,
                             sourcecoll, sample_acc, coldate, pt_name, txdate,
-                            donor_names, signed9 from demo""", one = False)
+                            donor_names, signed9 FROM demo""", one = False)
     return render_template('all_patients.html', entries = entries)
 
 @app.route('/<irs_id>', methods = ['GET', 'POST'])
 def indiv_results(irs_id):
     ids = str(irs_id)
-    entries = query_db("""select demo.irs_id, ptdon, sample_res, sample_type,
+    entries = query_db("""SELECT demo.irs_id, ptdon, sample_res, sample_type,
                             sourcecoll, sample_acc, coldate, pt_name, txdate,
                             donor_names, signed9, proj_id, proj_tube_no,
                             proj_cell, date_out, shipped_to, sent_to,
-                            received_date from demo, sample_movement where
+                            received_date FROM demo, sample_movement where
                             demo.irs_id = sample_movement.irs_id and
                             demo.irs_id = ?""", [ids])
     return render_template('get_results.html', entries = entries)
 
+#@app.route('/receive_samples')
+#def receive_samples():
+#    sample_id = request.form['sample_number']
+#    entries = query_db("""SELECT sample_info FROM base""", where id == 
 
 @app.route('/query')
 def query():
@@ -125,19 +128,25 @@ def query():
 @app.route('/results', methods = ['GET', 'POST'])
 def results():
     ids = str(request.form['irs_id'])
-    entries = query_db("""select demo.irs_id, ptdon, sample_res, sample_type,
+    entries = query_db("""SELECT demo.irs_id, ptdon, sample_res, sample_type,
                             sourcecoll, sample_acc, coldate, pt_name, txdate,
                             donor_names, signed9, proj_id, proj_tube_no,
                             proj_cell, date_out, shipped_to, sent_to,
-                            received_date from demo, sample_movement where
+                            received_date FROM demo, sample_movement where
                             demo.irs_id = sample_movement.irs_id and
                             demo.irs_id = ?""", [ids])
     return render_template('get_results.html', entries = entries)
 
+@app.route('/create_project', methods = ['GET', 'POST'])
+def create_project():
+            return render_template('create_project.html')
 
+@app.route('/send_samples', methods = ['GET', 'POST'])
+def send_samples():
+            return render_template('create_project.html')
 
-@app.route('/multiple_search', methods = ['GET', 'POST'])
-def multiple_search():
+@app.route('/submit_send', methods = ['GET', 'POST'])
+def submit_send():
     if request.method == 'POST':
         file = request.files['file']
         if file and allowed_file(file.filename):
@@ -146,35 +155,31 @@ def multiple_search():
             indivs = get_ids(os.path.join(app.config['UPLOAD_FOLDER'],
                 filename))
             print os.path.join(app.config['UPLOAD_FOLDER'],filename)
+            print request.form['sent_to']
             entries = query_db("""SELECT * FROM sample_movement WHERE irs_id IN
                     (%s)""" % ','.join('?'*len(indivs)), indivs)
-            return render_template('ms_results.html', entries=entries)
+            return render_template('send_samples.html', entries=entries)
 
-    return '''
-    <!doctype html>
-    <title>Upload CSVs</title>
-    <h1>Upload new CSV or txt file of interest</h1>
-    <form action="" method=post enctype=multipart/form-data>
-      <p><input type=file name=file> 
-      <input type=submit value=Upload>
-   </form>
-   <b>Note:</b> this query will only handle a CSV file with ID numbers in the first
-   column and less than 1000 ID numbers.
-   '''
+    return render_template('index.html')
+
 #test for facebook-style timeline
+
+@app.route('/ship_samples', methods = ['GET', 'POST'])
+def ship_samples():
+    pass
 
 @app.route('/test_movement', methods = ['GET', 'POST'])
 def test_movement():
 	error = None
-	entries = query_db("""select irs_id, max(datestamp),
-                        sent_from, datestamp, site from test_movement GROUP BY irs_id""", one = False ) 
+	entries = query_db("""SELECT irs_id, max(datestamp),
+                        sent_from, datestamp, site FROM test_movement GROUP BY irs_id""", one = False ) 
 	return render_template('test_movement.html', entries = entries)
 
 @app.route('/movement/<irs_id>', methods = ['GET', 'POST'])
 def test_indiv_results(irs_id):
     ids = str(irs_id)
-    entries = query_db("""select irs_id, datestamp, sent_from, site
-                            from test_movement where irs_id = ?""", [ids])
+    entries = query_db("""SELECT irs_id, datestamp, sent_from, site
+                            FROM test_movement where irs_id = ?""", [ids])
     return render_template('indiv_sample_timeline.html', entries = entries)
 
 @app.errorhandler(404)
